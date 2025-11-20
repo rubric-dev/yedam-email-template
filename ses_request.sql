c9d0d80fe169f5b62fd31e833332e2ed77c8f96d
이재형 <wogud1514@rubric.im>
2025-11-20 15:26:33 +0900

    aws 이메일 템플릿 요청 형태 추가

---
+++ ses_request.sql
@@ -0,0 +1,14 @@
+### 사실 sql 아님 form data로 요청
+
+templateName: PASSWORD_RESET
+subject: [Bridge+] 비밀번호 초기화
+
+templateName: SIGN_UP
+subject: [Bridge+] 회원가입
+
+templateName: INVITE
+subject: [Bridge+] 구성원 초대
+
+templateName: CONFIRM_MEMBER
+subject: [Bridge+] 계정 설정 및 약관 동의
+
